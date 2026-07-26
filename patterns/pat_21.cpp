bc21682f8fe0703799a8f01888bddbe08a925617
SKParthiv <parthiv.sk.pedapati@gmail.com>
2026-07-26 12:06:22 +0530

    pat_21

--- patterns/pat_21.cpp
+++ patterns/pat_21.cpp
@@ -8,15 +8,18 @@ int main() {
     for (int i = 0; i < n; i++) {
         cout << "*";
     }
-    cout << "\n";
     for (int i = 0; i < n; i++) {
-        if (i==0 && i==(n-1)) {
-            cout << "*";
+        cout << "\n";
+        for (int j = 0; j < n; j++) {
+            if (j==0 || j==(n-1)) {
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
         }
-        else {
-            cout << " ";
-        }
-    }
+   }
+    cout << "\n";
 
     for (int i = 0; i < n; i++) {
         cout << "*";
