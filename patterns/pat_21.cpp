a413ad338f721f11433348235b3a9f31f71e1cc6
SKParthiv <parthiv.sk.pedapati@gmail.com>
2026-07-26 11:59:04 +0530

    Proprietary Garbage(lol)

---
+++ patterns/pat_21.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << "*";
+    }
+    cout << "\n";
+    for (int i = 0; i < n; i++) {
+        if (i==0 && i==(n-1)) {
+            cout << "*";
+        }
+        else {
+            cout << " ";
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << "*";
+    }
+}
